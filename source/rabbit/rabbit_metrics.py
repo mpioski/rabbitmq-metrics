e92337c7b59454e7dde795bde420045caa572fc5
mpioski <matheus.henrique@pgmais.com.br>
2020-02-24 14:50:01 -0300

    Added put and delete methods. Added create vhost and create queue methods. Added delete messages and binding tests.

--- source/rabbit/rabbit_metrics.py
+++ source/rabbit/rabbit_metrics.py
@@ -4,6 +4,7 @@ from .api import Api
 
 
 class RabbitMetrics(Api):
+
     def get_info(self, name, json_type=True):
 
         url = "{0}/{1}".format(self.api, name)
@@ -22,3 +23,17 @@ class RabbitMetrics(Api):
             return response.text
 
         return response.json()
+
+    def put_info(self, name, data, json_type=True):
+
+        url = "{0}/{1}".format(self.api, name)
+        response = self.session.put(url=url, data=json.dumps(data))
+        if json_type:
+            return response.text
+
+        return response.json()
+
+    def delete_info(self, name, data):
+        url = "{0}/{1}".format(self.api, name)
+        response = self.session.delete(url=url, data=json.dumps(data))
+        return response.text
--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,24 @@ class TestMetrics(unittest.TestCase):
     def setUp(self) -> None:
         self.rabbit = RabbitMetrics()
         self.format = Format()
+        self.vhost = 'vhost_test'
+        self.queue = 'queue_test'
+        self.create_vhost()
+        self.create_queue()
+
+    def create_vhost(self):
+        endpoint = self.format.join_args('vhosts', self.vhost)
+
+        data = {"description": "test virtual host", "tags": "test"}
+        with self.rabbit:
+            self.rabbit.put_info(name=endpoint, data=data)
+
+    def create_queue(self):
+        endpoint = self.format.join_args('queues', self.vhost, self.queue)
+
+        data = {"auto_delete": False, "durable": True}
+        with self.rabbit:
+            self.rabbit.put_info(name=endpoint, data=data)
 
     def test_health(self):
         endpoint = "healthchecks/node"
@@ -25,9 +43,7 @@ class TestMetrics(unittest.TestCase):
         print(queues)
 
     def test_get_queue_messages(self):
-        vhost = 'vhost'
-        queue = 'queue'
-        endpoint = self.format.join_args('queues', vhost, queue, 'get')
+        endpoint = self.format.join_args('queues', self.vhost, self.queue, 'get')
 
         data = {"count": 5, "ackmode": "ack_requeue_true", "encoding": "auto", "truncate": 50000}
         with self.rabbit:
@@ -35,8 +51,25 @@ class TestMetrics(unittest.TestCase):
 
         print(queue_message)
 
+    def test_delete_messages(self):
+        endpoint = self.format.join_args('queues', self.vhost, self.queue, 'contents')
+
+        data = {"count": 5, "ackmode": "ack_requeue_true", "encoding": "auto", "truncate": 50000}
+        with self.rabbit:
+            r = self.rabbit.delete_info(name=endpoint, data=data)
+
+        print(r)
+
+    def test_bindings(self):
+        endpoint = self.format.join_args('queues', self.vhost, self.queue, 'bindings')
+
+        with self.rabbit:
+            bindings = self.rabbit.get_info(name=endpoint, json_type=True)
+
+        print(bindings)
+
 
 if __name__ == "__main__":
     t = TestMetrics()
     t.setUp()
-    t.test_get_queue_messages()
+    t.create_queue()
